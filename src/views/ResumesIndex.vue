b48b980f2d2d63acdf5226da7327f47dd62e4c1e
Brad Bell <brad.bb.bell@gmail.com>
2022-06-28 14:35:59 -0600

    Makes page display all dummy data

--- src/views/ResumesIndex.vue
+++ src/views/ResumesIndex.vue
@@ -6,51 +6,66 @@ export default {
     return {
       message: "Existing Resumes",
       student: {
-        personal: {
-          first_name: "James",
-          last_name: "Johnston",
-          email: "james@test.com",
-          phone_number: "123-456-7890",
-          short_bio: "This is a test student.",
-          linkedin: "linkedin.com/test",
-          twitter_handle: "twitter.com/test",
-          personal_website: "test.github.io",
-          online_resume: "test.resume.com",
-          github: "github.com/test",
-          photo: "placeholder.jpg",
-        },
-
+        first_name: "James",
+        last_name: "Johnston",
+        email: "james@test.com",
+        phone_number: "123-456-7890",
+        short_bio: "This is a test student.",
+        linkedin: "linkedin.com/test",
+        twitter_handle: "twitter.com/test",
+        personal_website: "test.github.io",
+        online_resume: "test.resume.com",
+        github: "github.com/test",
+        photo: "placeholder.jpg",
         experiences: [
-          { title: "Softwware Engineer", start_date: "2022-02-26", end_date: "", details: "Ran backend automation" },
-          { title: "Junior Softwware Engineer", start_date: "2020-01-26", end_date: "2022-01-20", details: "Dev Ops" },
+          {
+            company_name: "Company",
+            job_title: "Softwware Engineer",
+            start_date: "2022-02-26",
+            end_date: "current",
+            details: "Ran backend automation",
+          },
+          {
+            company_name: "Other Company",
+            job_title: "Junior Softwware Engineer",
+            start_date: "2020-01-26",
+            end_date: "2022-01-20",
+            details: "Dev Ops",
+          },
         ],
-
         educations: [
           {
-            university: "Dartmouth College",
+            university_name: "Dartmouth College",
             degree: "Bachelor of Arts",
             start_date: "2012-08-15",
             end_date: "2016-06-14",
             details: "James got his degree in economics and Japanese history",
           },
           {
-            university: "Actualize Coding Bootcamp",
+            university_name: "Actualize Coding Bootcamp",
             degree: "Coding Certificate",
             start_date: "2022-04-28",
             end_date: "2022-07-15",
             details: "James got his certificate in computer science",
           },
         ],
-
         skills: ["Ruby", "Vue.js", "Japanese", "JavaScript"],
-        capstone: [
-          { name: "Basketfy", description: "Thematic Investing Platform", url: "www.basketfy.io", screenshot: "hello" },
-        ],
+        capstone: {
+          name: "Basketfy",
+          description: "Thematic Investing Platform",
+          url: "www.basketfy.io",
+          screenshot: "hello",
+        },
       },
       resumeParams: {},
     };
   },
-  created: function () {},
+  created: function () {
+    console.log(this.student);
+    console.log(this.student.experiences);
+    console.log(this.student.educations);
+    console.log(this.student.capstone.name);
+  },
   methods: {
     showStudent: function () {
       axios.get("/students/1.json").then((response) => {
@@ -101,41 +116,40 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div>
-      <h2>Experience:</h2>
-      <h5>
-        Company Name:
-        <!-- <input type="text" v-model="student.experience.start_date" /> -->
-      </h5>
-      <h5>Job Title: {{ "student.experience.end_date" }}</h5>
-      <h5>Start Date:{{ "student.experience.job_title" }}</h5>
-      <h5>End Date: {{ "student.experience.company_name" }}</h5>
-      <h5>Details: {{ "student.experience.details" }}</h5>
+    <h2>Experience:</h2>
+    <div v-for="experience in student.experiences" v-bind:key="experience.id">
+      <h5>Company Name: {{ experience.company_name }}</h5>
+      <h5>Job Title: {{ experience.job_title }}</h5>
+      <h5>Start Date:{{ experience.start_date }}</h5>
+      <h5>End Date: {{ experience.end_date }}</h5>
+      <h5>Details: {{ experience.details }}</h5>
       <button v-on:click="editModalExperiences(resume)">Edit</button>
       <button v-on:click="addModalExperiences(resume)">Add</button>
     </div>
     <div>
       <h2>Education:</h2>
-      <h5>University: student.educations.start_date</h5>
-      <h5>Degree: student.educations.end_date</h5>
-      <h5>Start Date: student.educations.degree</h5>
-      <h5>End Date: student.educations.university_name</h5>
-      <h5>Details: student.educations.details</h5>
-      <button v-on:click="editModalEducation(resume)">Edit</button>
-      <button v-on:click="addModalEducation(resume)">Add</button>
+      <div v-for="education in student.educations" v-bind:key="education.id">
+        <h5>University Name: {{ education.university_name }}</h5>
+        <h5>Degree: {{ education.degree }}</h5>
+        <h5>Start Date: {{ education.start_date }}</h5>
+        <h5>End Date: {{ education.end_date }}</h5>
+        <h5>Details: {{ education.details }}</h5>
+        <button v-on:click="editModalEducation(resume)">Edit</button>
+        <button v-on:click="addModalEducation(resume)">Add</button>
+      </div>
     </div>
     <div>
       <h2>Skills:</h2>
-      <h5>Skills: student.skills.skills</h5>
+      <h5>{{ student.skills }}</h5>
       <button v-on:click="editModalSkills(resume)">Edit</button>
       <!-- <button v-on:click="addModalSkills(resume)">Add</button> -->
     </div>
     <div>
       <h2>Capstone:</h2>
-      <h5>Name: student.capstone.name</h5>
-      <h5>Description: student.capstone.description</h5>
-      <h5>URL: student.capstone.url</h5>
-      <h5>Screenshot: student.capstone.screenshot</h5>
+      <h5>Name: {{ student.capstone.name }}</h5>
+      <h5>Description: {{ student.capstone.description }}</h5>
+      <h5>URL: {{ student.capstone.url }}</h5>
+      <h5>Screenshot: {{ student.capstone.screenshot }}</h5>
     </div>
     <button v-on:click="editModalCapstone(resume)">Edit</button>
   </div>
